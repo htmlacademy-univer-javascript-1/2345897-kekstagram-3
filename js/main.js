df658b1876d8be3d06ec4368395e37b7ce227d2f
LeeryLis <64995672+LeeryLis@users.noreply.github.com>
2023-03-30 16:12:10 +0300

    Complete module4-task1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 function getRandomPositiveInt(min, max) {
-    if (min >= 0 && min <= max)
-      return Math.round(Math.random() * (max - min) + min);
-    return -1
+  if (min >= 0 && min <= max){
+    return Math.round(Math.random() * (max - min) + min);
+  }
+  return -1;
 }
+
+function isStringLengthEnough(string, maxLength) {
+  return string.length <= maxLength;
+}
+
+const photos = Array.from({length: 25}, (element, i) => ({
+  id: i + 1,
+  url: `photos/${i + 1}.jpg`,
+  description: `Photo${i + 1}`,
+  likes: getRandomPositiveInt(15, 200),
+  comments: getRandomPositiveInt(0, 200)
+}));
+
