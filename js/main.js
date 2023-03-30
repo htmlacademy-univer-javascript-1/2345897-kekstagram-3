bc44dd5843fea906426867e5c542c467114a22ac
LeeryLis <64995672+LeeryLis@users.noreply.github.com>
2023-03-30 17:35:42 +0300

    Complete module5-task1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-function getRandomPositiveInt(min, max) {
-  if (min >= 0 && min <= max){
-    return Math.round(Math.random() * (max - min) + min);
-  }
-  return -1;
-}
+import {getPhotosArray} from './util.js';
 
-export function isStringLengthEnough(string, maxLength) {
-  return string.length <= maxLength;
-}
-
-export const photos = Array.from({length: 25}, (element, i) => ({
-  id: i + 1,
-  url: `photos/${i + 1}.jpg`,
-  description: `Photo${i + 1}`,
-  likes: getRandomPositiveInt(15, 200),
-  comments: getRandomPositiveInt(0, 200)
-}));
-
+getPhotosArray(25);
---
+++ js/util.js
@@ -0,0 +1,25 @@
+import {photosArray} from './data.js';
+import {Photo} from './data.js';
+
+function getRandomPositiveInt(min, max) {
+  if (min >= 0 && min <= max){
+    return Math.round(Math.random() * (max - min) + min);
+  }
+  return -1;
+}
+
+function isStringLengthEnough(string, maxLength) {
+  return string.length <= maxLength;
+}
+
+export function getPhotosArray(number) {
+  for (let i = 0; i < number; i++) {
+    photosArray[i] = new Photo(
+      i + 1,
+      `photos/${i + 1}.jpg`,
+      `Photo${i + 1}`,
+      getRandomPositiveInt(15, 200),
+      getRandomPositiveInt(0, 200)
+    );
+  }
+}
