0b4bc503641f5edaefc938ed16cccefffc8118f7
LeeryLis <64995672+LeeryLis@users.noreply.github.com>
2023-03-30 15:19:07 +0300

    Complete module2-task1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 function getRandomPositiveInt(min, max) {
-    if (min >= 0 && min <= max)
-      return Math.round(Math.random() * (max - min) + min);
-    return -1
+  if (min >= 0 && min <= max){
+    return Math.round(Math.random() * (max - min) + min);
+  }
+  return -1;
+}
+
+function isStringLengthEnough(string, maxLength) {
+  return string.length <= maxLength;
 }
